f6afc62fed1a98e3501f4f4ede07a1995202f860
Adarsha <adarsha.stha123@gmail.com>
2025-03-06 20:54:07 +0800

    about us added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./component/Navbar";
 import Homepage from "./component/homePage/Homepage";
 import Footer from "./component/Footer";
+import About from "./component/aboutPage/About";
 
 import Menu from "./component/menuPage/Menu";
 
@@ -13,7 +14,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/meroResturant" element={<Homepage />} />
-       
+        <Route path="/about" element={<About />} />
         <Route path="/menu" element={<Menu />} />
       </Routes>
       <Footer />
