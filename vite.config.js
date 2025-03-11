c3a9c54819f4625cfb2d7255dd5aad890074902d
Adarsha Shrestha <30285136+Shrestha01@users.noreply.github.com>
2025-03-11 09:31:04 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: "/meroResturant/",
+//   plugins: [react()],
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/meroResturant/",
+  base: "/",  // Serve from the root of the domain
   plugins: [react()],
 });
