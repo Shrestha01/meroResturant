ec5efb84be820cd6cb8a66907a8687deafa67b8c
Adarsha Shrestha <30285136+Shrestha01@users.noreply.github.com>
2025-03-11 10:53:34 +0800

    Update MenuFilter.jsx

--- src/component/menuPage/MenuFilter.jsx
+++ src/component/menuPage/MenuFilter.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect,useContext } from "react";
 import CardMenu from "./CardMenu"; // Assuming CardMenu component is already created
-import { CounterContext } from './Menu';
 
-const MenuFilter = (menu) => {
+
+const MenuFilter = ({menu}) => {
 
 
 
